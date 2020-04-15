2751bbbc09c134ecc4298a4575d28518d9d7f915
MrMini <voronenco000@gmail.com>
2020-04-15 15:34:52 +0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,49 @@
+<?php
+$directory = '/folder.jpg';
+$slesh = '/phpmangaviewer/';
+if ($handle = opendir('phpmangaviewer')) {
+    while (false !== ($file = readdir($handle)) && false !== ($file1 = readdir($handle))) { 
+        if ($file != "." && $file != ".." && $file1 != "." && $file1 != "..") { 
+
+            echo '"    
+            <table width="100%" align="center">
+            <tr>
+            <th>
+            <a href="'.$slesh.$file.'" border="0"  vlink="white" alink="white"><img src="'.$slesh.$file.$directory.'" width="250px">
+            <H1>'.$file.'<H1>
+            </a>
+            </th>
+            <th>
+            <a href="'.$slesh.$file1.'" border="0"  vlink="white" alink="white"><img src="'.$slesh.$file1.$directory.'" width="250px">
+            <H1>'.$file1.'<H1>
+            </a>
+            </th>
+            </tr>
+            </table>
+            "';
+        } 
+    }
+    
+    closedir($handle); 
+}
+
+?> 
+<html>
+    <title>Manga</title>
+    <head>
+      <style>   
+      @import url('https://fonts.googleapis.com/css?family=Ubuntu&display=swap');
+       body {
+           background-color: black;
+           font-family: 'Ubuntu', sans-serif;
+       }        
+            h1 {
+            font-family: 'Ubuntu', sans-serif;
+            font-size: 12px;
+            color: white;
+        }
+
+      </style>
+    </head>
+
+    </html>
